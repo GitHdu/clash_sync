85d39645074e5ce2a92e5ffda2f3009cdf8b62fc
Nobody <acc.4code@gmail.com>
2022-10-30 01:33:41 +0800

    Update func.sh

--- .github/func.sh
+++ .github/func.sh
@@ -9,6 +9,7 @@ getRemoteFile(){
       fileName=$(git diff --name-only --diff-filter=AMT "$repositoryName"/"$branchName"~1 "$repositoryName"/"$branchName"  | xargs -I{} -- git log -1  --remotes="$repositoryName"  --format="%ci {}" -- {} | sort | tail -1 | cut -d " " -f4)
    fi
   
+   echo "file--" $fileNamee
    git checkout $repositoryName/$branchName "$fileName"
    mv $fileName merge_$fileName
    git rm -rf "$fileName"
